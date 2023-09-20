c797089d525056febdaecb5fbf60f77a35987204
Rashed Alsaleh <114973231+r340i@users.noreply.github.com>
2023-09-20 14:17:43 +0300

    Delete parts directory

--- parts/footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-<script src="./js/bootstrap.bundle.min.js" ></script>
-<script src="./js/loader.js"></script>
-<script src="./js/confettei.js"></script>
-
-<script src="./js/script.js"></script>
-    </body>
-    </html>
